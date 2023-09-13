b5054514357479f21db993a8c46035ad648a7b00
Nkemjika Obi <nkemjikaobi@gmail.com>
2023-09-13 12:00:13 -0400

    Toggle Background

--- App.js
+++ App.js
@@ -1,20 +1,135 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+	Button,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+	Switch,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [value, setValue] = useState('');
+	const [toggleValue, setToggleValue] = useState(true);
+
+	const handleTextChange = newValue => {
+		setValue(newValue);
+	};
+
+	const toggleSwitch = () => setToggleValue(previousState => !previousState);
+
+	return (
+		<View
+			style={[
+				styles.container,
+				toggleValue ? { backgroundColor: '#000' } : { backgroundColor: '#fff' },
+			]}
+		>
+			<Text
+				style={[
+					styles.header,
+					toggleValue ? { color: '#fff' } : { color: '#000' },
+				]}
+			>
+				Basic App
+			</Text>
+			<TextInput
+				style={[
+					styles.textInput,
+					toggleValue
+						? { color: '#fff', borderColor: '#fff' }
+						: { color: '#000', borderColor: '#000' },
+				]}
+				value={value}
+				onChangeText={val => handleTextChange(val)}
+				placeholder='Enter any thing here...'
+				clearTextOnFocus={true}
+			/>
+			<View style={styles.outputContainer}>
+				<Text
+					style={[
+						styles.outputLabel,
+						toggleValue ? { color: '#fff' } : { color: '#000' },
+					]}
+				>
+					Output:
+				</Text>
+				<Text
+					style={[
+						styles.outputValue,
+						toggleValue ? { color: '#fff' } : { color: '#000' },
+					]}
+				>
+					{value}
+				</Text>
+			</View>
+			<Button
+				style={styles.clearBtn}
+				disabled={!value}
+				title='Clear Input'
+				onPress={() => handleTextChange('')}
+			/>
+			<View style={styles.toggleBgContainer}>
+				<Text
+					style={[
+						styles.toggleContainerLabel,
+						toggleValue ? { color: '#fff' } : { color: '#000' },
+					]}
+				>
+					Toggle Background Color:
+				</Text>
+				<Switch onValueChange={toggleSwitch} value={toggleValue} />
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	header: {
+		fontSize: 32,
+		marginBottom: 20,
+	},
+	textInput: {
+		borderWidth: 1,
+		borderRadius: 8,
+		width: '80%',
+		height: 40,
+		padding: 8,
+	},
+	outputContainer: {
+		margin: 20,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	outputLabel: {},
+	outputValue: {
+		marginLeft: 8,
+	},
+	clearBtn: {
+		width: 200,
+		height: 40,
+		backgroundColor: 'black',
+		fontSize: 24,
+		borderWidth: 1,
+		borderRadius: 8,
+	},
+	toggleContainerLabel: {
+		fontSize: 14,
+		marginRight: 20,
+	},
+	toggleBgContainer: {
+		margin: 20,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 });
